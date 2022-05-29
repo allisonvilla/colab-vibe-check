345c64417071fe98386c453489e5245f80e08720
allison <allison.villa@outlook.com>
2022-05-28 21:00:33 -0400

    conditionally rendering nav bar icon colour based on route

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { ReactComponent as NavSwipe } from "../../assets/nav-swipe.svg";
 import { ReactComponent as NavMatches } from "../../assets/nav-matches.svg";
 import { ReactComponent as NavProfile } from "../../assets/nav-profile.svg";
+import React from "react";
 
 const NavBar = () => {
-  const currentPage = () => {};
+  const [swipeColour, setSwipeColour] = useState("");
+  const [matchesColour, setMatchesColour] = useState("");
+  const [profileColour, setProfileColour] = useState("");
+
+  const currentPage = useLocation();
+
+  useEffect(() => {
+    setSwipeColour("none");
+    setMatchesColour("none");
+    setProfileColour("none");
+
+    if (currentPage.pathname === "/") {
+      setProfileColour("red");
+    } else if (currentPage.pathname === "/matches") {
+      setMatchesColour("red");
+    } else if (currentPage.pathname === "/swipe") {
+      setSwipeColour("red");
+    }
+  }, [currentPage]);
 
   return (
     <nav className={styles.container}>
       <Link to="/">
-        <NavSwipe />
+        <NavSwipe stroke={swipeColour} />
       </Link>
       <Link to="/">
-        <NavMatches />
+        <NavMatches stroke={matchesColour} />
       </Link>
       <Link to="/">
-        why
-        <NavProfile />
+        <NavProfile stroke={profileColour} />
       </Link>
     </nav>
   );
