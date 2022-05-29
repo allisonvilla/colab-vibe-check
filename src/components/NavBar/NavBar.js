26f3e55de158f7680e4f543274c395b84f1a4e81
allison <allison.villa@outlook.com>
2022-05-28 20:36:47 -0400

    nav bar svg problems

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
+import { ReactComponent as NavSwipe } from "../../assets/nav-swipe.svg";
+import { ReactComponent as NavMatches } from "../../assets/nav-matches.svg";
+import { ReactComponent as NavProfile } from "../../assets/nav-profile.svg";
 
 const NavBar = () => {
+  const currentPage = () => {};
+
   return (
     <nav className={styles.container}>
-      <p>Navigation Bar goes here</p>
+      <Link to="/">
+        <NavSwipe />
+      </Link>
+      <Link to="/">
+        <NavMatches />
+      </Link>
+      <Link to="/">
+        why
+        <NavProfile />
+      </Link>
     </nav>
   );
 };
