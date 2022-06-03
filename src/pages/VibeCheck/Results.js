c7f010609360e4fdd5cd57b2185d444a15f163dd
allison <allison.villa@outlook.com>
2022-06-03 10:25:59 -0400

    successfully showing correct dater type in results

--- src/pages/VibeCheck/Results.js
+++ src/pages/VibeCheck/Results.js
@@ -4,13 +4,20 @@ const Results = () => {
   return (
     <VibeCheckContext.Consumer>
       {({ possibleOutcomes }) => {
+        let highestCount = 0;
+        let daterType;
+
+        console.log(possibleOutcomes);
+
         for (const type in possibleOutcomes) {
-          return (
-            <p>
-              {type}: {possibleOutcomes[type]}
-            </p>
-          );
+          if (possibleOutcomes[type] > highestCount) {
+            highestCount = possibleOutcomes[type];
+            daterType = type;
+          }
         }
+        console.log(`Highest Count: ${highestCount}`);
+        console.log(`Final Dater Type: ${daterType}`);
+        return <p>Your dater type is '{daterType}'!</p>;
       }}
     </VibeCheckContext.Consumer>
   );
