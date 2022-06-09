8038b735586de3b32e6cc9fdcd0af4532ab1f77f
allison <allison.villa@outlook.com>
2022-06-09 19:59:34 -0400

    reverted the way results declaration is shown to match updated design

--- src/pages/VibeCheck/Results/Results.js
+++ src/pages/VibeCheck/Results/Results.js
@@ -4,26 +4,19 @@ import { useContext, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 const Results = () => {
-  const { setQuizData, quizData, setUserData } = useContext(UserContext);
+  const { setQuizData, quizData, setUserData, userData } = useContext(UserContext);
   const { possibleOutcomes } = useContext(VibeCheckContext);
 
   const [daterType, setDaterType] = useState("");
 
   const [description, setDescription] = useState("");
-  const [sentence, setSentence] = useState("");
+
   const [compCollapsed, setCompCollapsed] = useState(true);
   const [convCollapsed, setConvCollapsed] = useState(true);
   const [compatibilityQs, setCompatibilityQs] = useState([]);
   const [conversationQs, setConversationQs] = useState([]);
 
   useEffect(() => {
-    const results = {
-      seriousDater: `You are a serious dater!`,
-      casualDater: `You are a casual dater!`,
-      goWithTheFlow: `You are a "Go with the flow" dater!`,
-      workingOnMyself: `You are working on yourself!`
-    };
-
     const descriptions = {
       seriousDater: `You're looking to form a meaningful romantic connection with someone.`,
       casualDater: `You're not looking for anything serious, you just want to have some fun.`,
@@ -37,8 +30,9 @@ const Results = () => {
       if (possibleOutcomes[type] > highestCount) {
         highestCount = possibleOutcomes[type];
         setDescription(descriptions[type]);
-        setSentence(results[type]);
-        setDaterType(type);
+        setDaterType(
+          type.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/^./, (str) => str.toUpperCase())
+        );
         //   } else if (possibleOutcomes[type] === highestCount) {
         //     daterType = "Go With The Flow";
       }
@@ -75,8 +69,8 @@ const Results = () => {
 
   return (
     <div className={styles.container}>
-      <h1>Your Quiz Result</h1>
-      <h2>{sentence}</h2>
+      <h2>Your dater type result is:</h2>
+      <h1>{userData.daterType}</h1>
       {/* Image goes here */}
       <p className={styles.description}>{description}</p>
       <div>
