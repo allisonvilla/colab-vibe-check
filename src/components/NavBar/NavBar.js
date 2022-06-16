e1db399aa9687607e1dcb5f973ae3cb062a76257
allison <allison.villa@outlook.com>
2022-06-16 18:36:59 -0400

    updated svg assets and focus colour for nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
-import { ReactComponent as NavSwipe } from "assets/nav-swipe.svg";
-import { ReactComponent as NavMatches } from "assets/nav-matches.svg";
-import { ReactComponent as NavProfile } from "assets/nav-profile.svg";
+import { ReactComponent as NavSwipe } from "assets/nav/swipe.svg";
+import { ReactComponent as NavMatches } from "assets/nav/fire.svg";
+import { ReactComponent as NavProfile } from "assets/nav/profile.svg";
 
 const NavBar = () => {
   const [swipeColour, setSwipeColour] = useState("");
@@ -13,16 +13,18 @@ const NavBar = () => {
   const currentPage = useLocation();
 
   useEffect(() => {
+    const focusColour = "#F64900";
+
     setSwipeColour("none");
     setMatchesColour("none");
     setProfileColour("none");
 
     if (currentPage.pathname === "/") {
-      setProfileColour("red");
+      setProfileColour(focusColour);
     } else if (currentPage.pathname === "/matches") {
-      setMatchesColour("red");
+      setMatchesColour(focusColour);
     } else if (currentPage.pathname === "/swipe") {
-      setSwipeColour("red");
+      setSwipeColour(focusColour);
     }
   }, [currentPage]);
 
