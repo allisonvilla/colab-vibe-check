1a7faf5e95ed6cd2613e006e84729332c5fc706f
allison <allison.villa@outlook.com>
2022-06-15 22:17:51 -0400

    removed unused pages and routes

--- src/routes/routesList.js
+++ src/routes/routesList.js
@@ -2,8 +2,6 @@ import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 
 const Home = lazy(() => import("pages/Home/Home"));
-const About = lazy(() => import("pages/About/About"));
-const UserProfile = lazy(() => import("pages/UserProfile/UserProfile"));
 const VibeCheck = lazy(() => import("pages/VibeCheck/VibeCheck"));
 const Results = lazy(() => import("pages/Results/Results"));
 
@@ -12,16 +10,6 @@ const routes = [
     path: "/",
     exact: true,
     component: Home
-  },
-  {
-    path: "/about",
-    exact: true,
-    component: About
-  },
-  {
-    path: "/myprofile",
-    exact: true,
-    component: UserProfile
   },
   {
     path: "/vibecheck",
