1b616845113860be8865d8e0ddf578afbaf063a9
allison <allison.villa@outlook.com>
2022-06-16 19:30:18 -0400

    refactored nav links and conditional styles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,44 +1,25 @@
 import { Link, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { ReactComponent as NavSwipe } from "assets/nav/swipe.svg";
 import { ReactComponent as NavMatches } from "assets/nav/fire.svg";
 import { ReactComponent as NavProfile } from "assets/nav/profile.svg";
 
 const NavBar = () => {
-  const [swipeColour, setSwipeColour] = useState("");
-  const [matchesColour, setMatchesColour] = useState("");
-  const [profileColour, setProfileColour] = useState("");
-
   const currentPage = useLocation();
 
-  useEffect(() => {
-    const focusColour = "#F64900";
-
-    setSwipeColour("none");
-    setMatchesColour("none");
-    setProfileColour("none");
-
-    if (currentPage.pathname === "/") {
-      setProfileColour(focusColour);
-    } else if (currentPage.pathname === "/matches") {
-      setMatchesColour(focusColour);
-    } else if (currentPage.pathname === "/swipe") {
-      setSwipeColour(focusColour);
-    }
-  }, [currentPage]);
+  const links = [
+    { type: `swipe`, icon: <NavSwipe />, link: `/swipe` },
+    { type: `matches`, icon: <NavMatches />, link: `/matches` },
+    { type: `profile`, icon: <NavProfile />, link: `/` }
+  ];
 
   return (
     <nav className={styles.container}>
-      <Link to="/">
-        <NavSwipe stroke={swipeColour} />
-      </Link>
-      <Link to="/">
-        <NavMatches stroke={matchesColour} />
-      </Link>
-      <Link to="/">
-        <NavProfile stroke={profileColour} />
-      </Link>
+      {links.map((i) => (
+        <Link to={i.link} className={currentPage.pathname === i.link ? styles.focused : null}>
+          {i.icon}
+        </Link>
+      ))}
     </nav>
   );
 };
