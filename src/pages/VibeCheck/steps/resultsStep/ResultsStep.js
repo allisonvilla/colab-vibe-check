4c4fc65094dcc5a15a33f94b2d3135ac2d2b9e07
allison <allison.villa@outlook.com>
2022-06-15 21:36:35 -0400

    different view when seeing results after locking them in

--- src/pages/VibeCheck/steps/resultsStep/ResultsStep.js
+++ src/pages/VibeCheck/steps/resultsStep/ResultsStep.js
@@ -1,14 +1,24 @@
 import { UserContext } from "App";
 import { VibeCheckContext } from "pages/VibeCheck/VibeCheck";
 import { useContext, useEffect, useState } from "react";
-import Results from "pages/Results/Results";
+import { Link, useNavigate } from "react-router-dom";
+import { HiOutlineChevronDoubleRight } from "react-icons/hi";
+import styles from "pages/Results/styles.module.scss";
 
 const ResultsStep = () => {
-  const { setUserData, userData } = useContext(UserContext);
+  const { setUserData, userData, quizData, setQuizData } = useContext(UserContext);
   const { possibleOutcomes } = useContext(VibeCheckContext);
 
   const [daterType, setDaterType] = useState("");
   const [description, setDescription] = useState("");
+
+  const [compCollapsed, setCompCollapsed] = useState(true);
+  const [convCollapsed, setConvCollapsed] = useState(true);
+  const [compatibilityQs, setCompatibilityQs] = useState([]);
+  const [conversationQs, setConversationQs] = useState([]);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const descriptions = {
@@ -38,14 +48,228 @@ const ResultsStep = () => {
     });
   }, [daterType, description, possibleOutcomes, setUserData]);
 
+  useEffect(() => {
+    const compArr = [];
+    const convArr = [];
+
+    for (const q in quizData) {
+      if (quizData[q].type !== "noEffect") {
+        compArr.push(quizData[q]);
+      } else {
+        convArr.push(quizData[q]);
+      }
+
+      if (quizData[q].show) {
+        setSelectedAnswers((prevState) => {
+          let newState = [...prevState];
+          newState.push(quizData[q]);
+          return newState;
+        });
+      }
+    }
+
+    setCompatibilityQs(compArr);
+    setConversationQs(convArr);
+  }, [quizData]);
+
+  const handleExpand = (list) => {
+    if (list === "compatibility") {
+      setCompCollapsed(!compCollapsed);
+    } else if (list === "conversation") {
+      setConvCollapsed(!convCollapsed);
+    }
+  };
+
+  const handleSelect = (ans) => {
+    if (selectedAnswers.includes(ans)) {
+      setSelectedAnswers((prevState) => {
+        let newState = [...prevState];
+        let filteredArr = newState.filter((i) => i !== ans);
+        return filteredArr;
+      });
+    } else {
+      setSelectedAnswers((prevState) => {
+        let newState = [...prevState];
+        newState.push(ans);
+        return newState;
+      });
+    }
+  };
+
+  const handleSelectAll = (arr) => {
+    arr.forEach((q) => {
+      if (!selectedAnswers.includes(q)) {
+        setSelectedAnswers((prevState) => {
+          let newState = [...prevState];
+          newState.push(q);
+          return newState;
+        });
+      }
+    });
+  };
+
+  const saveSelected = () => {
+    for (const q in quizData) {
+      if (selectedAnswers.includes(quizData[q])) {
+        setQuizData((prevState) => {
+          let newState = { ...prevState };
+          selectedAnswers.forEach((ans) => {
+            newState[`q${ans.key}`].show = true;
+          });
+          return newState;
+        });
+      } else {
+        setQuizData((prevState) => {
+          let newState = { ...prevState };
+          newState[q].show = false;
+          return newState;
+        });
+      }
+    }
+  };
+
+  const images = {
+    seriousDater: {
+      path: require("assets/results/seriousDater.png"),
+      alt: "A man proposing to a woman."
+    },
+    casualDater: {
+      path: require("assets/results/casualDater.png"),
+      alt: "A couple sitting on the couch while cheering."
+    },
+    goWithTheFlow: {
+      path: require("assets/results/goWithTheFlow.png"),
+      alt: "A couple enjoying a picnic in the sun."
+    },
+    workingOnMyself: {
+      path: require("assets/results/workingOnMyself.png"),
+      alt: "A woman sitting on a chair, reading a book."
+    }
+  };
+
   if (userData.daterType && userData.daterDesc) {
-    return <Results />;
+    return (
+      <div className={styles.container}>
+        <h2>Your dater type result is:</h2>
+        <h1>
+          {userData.daterType
+            .replace(/([a-z])([A-Z])/g, "$1 $2")
+            .replace(/^./, (str) => str.toUpperCase())}
+        </h1>
+
+        <img
+          className={styles.resultImage}
+          src={images[userData.daterType].path}
+          alt={images[userData.daterType].alt}
+        />
+
+        <p className={styles.description}>{userData.daterDesc}</p>
+        <p className={styles.instruction}>Add your answers to your profile below!</p>
+
+        <div className={styles.questionSection}>
+          <div className={styles.questionHeader}>
+            <h4>Compatibility Questions</h4>
+            <button
+              onClick={() => {
+                handleExpand("compatibility");
+              }}
+            >
+              {compCollapsed ? (
+                <img src={require("assets/results/plus.png")} alt="Plus icon" />
+              ) : (
+                <img src={require("assets/results/minus.png")} alt="Minus icon" />
+              )}
+            </button>
+          </div>
+          <ul className={compCollapsed ? styles.collapsed : null}>
+            <button
+              onClick={() => {
+                handleSelectAll(compatibilityQs);
+              }}
+              className={styles.selectAllBtn}
+            >
+              Select All
+            </button>
+            {compatibilityQs.map((item) => (
+              <li key={item.key}>
+                <button
+                  onClick={() => {
+                    handleSelect(item);
+                  }}
+                >
+                  {/* need assets */}
+                  {selectedAnswers.includes(item) ? `✅` : `❎`}
+                </button>
+                <div className={styles.qa}>
+                  <p className={styles.question}>{item.question}</p>
+                  <p className={styles.answer}>{item.answer}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.questionSection}>
+          <div className={styles.questionHeader}>
+            <h4>Conversation Starters</h4>
+            <button
+              onClick={() => {
+                handleExpand("conversation");
+              }}
+            >
+              {convCollapsed ? (
+                <img src={require("assets/results/plus.png")} alt="Plus icon" />
+              ) : (
+                <img src={require("assets/results/minus.png")} alt="Minus icon" />
+              )}
+            </button>
+          </div>
+          <ul className={convCollapsed ? styles.collapsed : null}>
+            <button
+              onClick={() => {
+                handleSelectAll(conversationQs);
+              }}
+              className={styles.selectAllBtn}
+            >
+              Select All
+            </button>
+            {conversationQs.map((item) => (
+              <li key={item.key}>
+                <button
+                  onClick={() => {
+                    handleSelect(item);
+                  }}
+                >
+                  {/* need assets */}
+                  {selectedAnswers.includes(item) ? `✅` : `❎`}
+                </button>
+                <div className={styles.qa}>
+                  <p className={styles.question}>{item.question}</p>
+                  <p className={styles.answer}>{item.answer}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.addAnswers}>
+          <p>Note: Selected answers will be added to profile</p>
+          {/* need assets */}
+          <button
+            onClick={() => {
+              saveSelected();
+              navigate("/");
+            }}
+          >
+            Add answers to profile <HiOutlineChevronDoubleRight />
+          </button>
+          <Link to="/">Cancel</Link>
+        </div>
+      </div>
+    );
   } else {
     return <></>;
   }
 };
-
-// TODO:
-// Fix the tie logic
 
 export default ResultsStep;
